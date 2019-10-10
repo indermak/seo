e688b24b7526bce730cdf2a1f47ae5f6188abb49
iitpl_inderjeetsingh <inderjeet.singh@bajajcapital.com>
2019-10-10 22:38:47 +0530

    Updates

---
+++ precache-manifest.0dff1a5a4b214a17a2deb1f704a12f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87db0598515b79070c589d95e3d7dc1",
+    "url": "/seo/index.html"
+  },
+  {
+    "revision": "0134b61da0f31a28470c",
+    "url": "/seo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "21503e424b22c4989adc",
+    "url": "/seo/static/js/2.9bc3a574.chunk.js"
+  },
+  {
+    "revision": "0134b61da0f31a28470c",
+    "url": "/seo/static/js/main.b7a5eb5b.chunk.js"
+  },
+  {
+    "revision": "cefc8643a2a4593a86a5",
+    "url": "/seo/static/js/runtime-main.f58e7209.js"
+  }
+]);
